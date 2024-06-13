eda1fa3a6e8104399f49e24e83340a6e5258b3c6
jacksonmckee <jmckee50@qub.ac.uk>
2024-06-13 18:49:58 +0100

    connected to db using .env

---
+++ learninglibrary-api/server.js
@@ -0,0 +1,24 @@
+require('dotenv').config();
+const express = require("express");
+const app = express();
+const mysql = require('mysql2');
+
+const PORT = process.env.PORT || 4000;
+app.set('view engine', 'ejs');
+
+const connection = mysql.createPool({
+    host: process.env.DB_HOST,
+    user: process.env.DB_USER,
+    password: process.env.DB_PW,
+    database: process.env.DB_NAME,
+    waitForConnections: true,
+    connectionLimit: 10,
+    queueLimit:10,
+    port: process.env.DB_PORT,
+    multipleStatements: true
+});
+
+connection.getConnection((err)=>{
+    if(err) return console.log(err.message);
+    console.log("connected to local mysql db using .env properties");
+});
