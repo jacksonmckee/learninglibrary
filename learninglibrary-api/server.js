84f14effa5dbf5f1b3caf27d0504fd7532abd825
jacksonmckee <jmckee50@qub.ac.uk>
2024-06-15 21:35:30 +0100

    displaying resources by topic and media type on api

--- learninglibrary-api/server.js
+++ learninglibrary-api/server.js
@@ -189,6 +189,381 @@ app.get('/api-allpodcasts', (req, res) => {
 
 });
 
+// All react videos //
+
+app.get('/api-allreactvideos', (req, res) => {
+
+    let allreactvideos = `SELECT *
+                    FROM resource
+                    WHERE media_id = 1 AND topic_id = 1`;
+
+    connection.query(allreactvideos, (err, data) => {
+        if (err) throw err;
+        res.json({ data });
+    });
+
+});
+
+// All react webpages //
+
+app.get('/api-allreactwebpages', (req, res) => {
+
+    let allreactwebpages = `SELECT *
+                    FROM resource
+                    WHERE media_id = 2 AND topic_id = 1`;
+
+    connection.query(allreactwebpages, (err, data) => {
+        if (err) throw err;
+        res.json({ data });
+    });
+
+});
+
+// All react online courses //
+
+app.get('/api-allreactcourses', (req, res) => {
+
+    let allreactcourses = `SELECT *
+                    FROM resource
+                    WHERE media_id = 3 AND topic_id = 1`;
+
+    connection.query(allreactcourses, (err, data) => {
+        if (err) throw err;
+        res.json({ data });
+    });
+
+});
+
+// All react books //
+
+app.get('/api-allreactbooks', (req, res) => {
+
+    let allreactbooks = `SELECT *
+                    FROM resource
+                    WHERE media_id = 4 AND topic_id = 1`;
+
+    connection.query(allreactbooks, (err, data) => {
+        if (err) throw err;
+        res.json({ data });
+    });
+
+});
+
+// All react podcasts //
+
+app.get('/api-allreactpodcasts', (req, res) => {
+
+    let allreactpodcasts = `SELECT *
+                    FROM resource
+                    WHERE media_id = 5 AND topic_id = 1`;
+
+    connection.query(allreactpodcasts, (err, data) => {
+        if (err) throw err;
+        res.json({ data });
+    });
+
+});
+
+// All html videos //
+
+app.get('/api-allhtmlvideos', (req, res) => {
+
+    let allhtmlvideos = `SELECT *
+                    FROM resource
+                    WHERE media_id = 1 AND topic_id = 2`;
+
+    connection.query(allhtmlvideos, (err, data) => {
+        if (err) throw err;
+        res.json({ data });
+    });
+
+});
+
+// All html webpages //
+
+app.get('/api-allhtmlwebpages', (req, res) => {
+
+    let allhtmlwebpages = `SELECT *
+                    FROM resource
+                    WHERE media_id = 2 AND topic_id = 2`;
+
+    connection.query(allhtmlwebpages, (err, data) => {
+        if (err) throw err;
+        res.json({ data });
+    });
+
+});
+
+// All html courses //
+
+app.get('/api-allhtmlcourses', (req, res) => {
+
+    let allhtmlcourses = `SELECT *
+                    FROM resource
+                    WHERE media_id = 3 AND topic_id = 2`;
+
+    connection.query(allhtmlcourses, (err, data) => {
+        if (err) throw err;
+        res.json({ data });
+    });
+
+});
+
+// All html books //
+
+app.get('/api-allhtmlbooks', (req, res) => {
+
+    let allhtmlbooks = `SELECT *
+                    FROM resource
+                    WHERE media_id = 4 AND topic_id = 2`;
+
+    connection.query(allhtmlbooks, (err, data) => {
+        if (err) throw err;
+        res.json({ data });
+    });
+
+});
+
+// All html podcasts //
+
+app.get('/api-allhtmlpodcasts', (req, res) => {
+
+    let allhtmlpodcasts = `SELECT *
+                    FROM resource
+                    WHERE media_id = 5 AND topic_id = 2`;
+
+    connection.query(allhtmlpodcasts, (err, data) => {
+        if (err) throw err;
+        res.json({ data });
+    });
+
+});
+
+// All gardening videos //
+
+app.get('/api-allgardeningvideos', (req, res) => {
+
+    let allgardeningvideos = `SELECT *
+                    FROM resource
+                    WHERE media_id = 1 AND topic_id = 3`;
+
+    connection.query(allgardeningvideos, (err, data) => {
+        if (err) throw err;
+        res.json({ data });
+    });
+
+});
+
+// All gardening webpages //
+
+app.get('/api-allgardeningwebpages', (req, res) => {
+
+    let allgardeningwebpages = `SELECT *
+                    FROM resource
+                    WHERE media_id = 2 AND topic_id = 3`;
+
+    connection.query(allgardeningwebpages, (err, data) => {
+        if (err) throw err;
+        res.json({ data });
+    });
+
+});
+
+// All gardening courses //
+
+app.get('/api-allgardeningcourses', (req, res) => {
+
+    let allgardeningcourses = `SELECT *
+                    FROM resource
+                    WHERE media_id = 3 AND topic_id = 3`;
+
+    connection.query(allgardeningcourses, (err, data) => {
+        if (err) throw err;
+        res.json({ data });
+    });
+
+});
+
+// All gardening books (there is 0) //
+
+app.get('/api-allgardeningbooks', (req, res) => {
+
+    let allgardeningbooks = `SELECT *
+                    FROM resource
+                    WHERE media_id = 4 AND topic_id = 3`;
+
+    connection.query(allgardeningbooks, (err, data) => {
+        if (err) throw err;
+        res.json({ data });
+    });
+
+});
+
+// All gardening podcasts //
+
+app.get('/api-allgardeningpodcasts', (req, res) => {
+
+    let allgardeningpodcasts = `SELECT *
+                    FROM resource
+                    WHERE media_id = 5 AND topic_id = 3`;
+
+    connection.query(allgardeningpodcasts, (err, data) => {
+        if (err) throw err;
+        res.json({ data });
+    });
+
+});
+
+// All spanish videos (there is 0) //
+
+app.get('/api-allspanishvideos', (req, res) => {
+
+    let allspanishvideos = `SELECT *
+                    FROM resource
+                    WHERE media_id = 1 AND topic_id = 4`;
+
+    connection.query(allspanishvideos, (err, data) => {
+        if (err) throw err;
+        res.json({ data });
+    });
+
+});
+
+// All spanish webpages //
+
+app.get('/api-allspanishwebpages', (req, res) => {
+
+    let allspanishwebpages = `SELECT *
+                    FROM resource
+                    WHERE media_id = 2 AND topic_id = 4`;
+
+    connection.query(allspanishwebpages, (err, data) => {
+        if (err) throw err;
+        res.json({ data });
+    });
+
+});
+
+// All spanish courses //
+
+app.get('/api-allspanishcourses', (req, res) => {
+
+    let allspanishcourses = `SELECT *
+                    FROM resource
+                    WHERE media_id = 3 AND topic_id = 4`;
+
+    connection.query(allspanishcourses, (err, data) => {
+        if (err) throw err;
+        res.json({ data });
+    });
+
+});
+
+// All spanish books //
+
+app.get('/api-allspanishbooks', (req, res) => {
+
+    let allspanishbooks = `SELECT *
+                    FROM resource
+                    WHERE media_id = 4 AND topic_id = 4`;
+
+    connection.query(allspanishbooks, (err, data) => {
+        if (err) throw err;
+        res.json({ data });
+    });
+
+});
+
+// All spanish podcasts //
+
+app.get('/api-allspanishpodcasts', (req, res) => {
+
+    let allspanishpodcasts = `SELECT *
+                    FROM resource
+                    WHERE media_id = 5 AND topic_id = 4`;
+
+    connection.query(allspanishpodcasts, (err, data) => {
+        if (err) throw err;
+        res.json({ data });
+    });
+
+});
+
+// All plumbing videos //
+
+app.get('/api-allplumbingvideos', (req, res) => {
+
+    let allplumbingvideos = `SELECT *
+                    FROM resource
+                    WHERE media_id = 1 AND topic_id = 5`;
+
+    connection.query(allplumbingvideos, (err, data) => {
+        if (err) throw err;
+        res.json({ data });
+    });
+
+});
+
+// All plumbing webpages //
+
+app.get('/api-allplumbingwebpages', (req, res) => {
+
+    let allplumbingwebpages = `SELECT *
+                    FROM resource
+                    WHERE media_id = 2 AND topic_id = 5`;
+
+    connection.query(allplumbingwebpages, (err, data) => {
+        if (err) throw err;
+        res.json({ data });
+    });
+
+});
+
+// All plumbing courses //
+
+app.get('/api-allplumbingcourses', (req, res) => {
+
+    let allplumbingcourses = `SELECT *
+                    FROM resource
+                    WHERE media_id = 3 AND topic_id = 5`;
+
+    connection.query(allplumbingcourses, (err, data) => {
+        if (err) throw err;
+        res.json({ data });
+    });
+
+});
+
+// All plumbing books //
+
+app.get('/api-allplumbingbooks', (req, res) => {
+
+    let allplumbingbooks = `SELECT *
+                    FROM resource
+                    WHERE media_id = 4 AND topic_id = 5`;
+
+    connection.query(allplumbingbooks, (err, data) => {
+        if (err) throw err;
+        res.json({ data });
+    });
+
+});
+
+// All plumbing podcasts (there are 0) //
+
+app.get('/api-allplumbingpodcasts', (req, res) => {
+
+    let allplumbingpodcasts = `SELECT *
+                    FROM resource
+                    WHERE media_id = 5 AND topic_id = 5`;
+
+    connection.query(allplumbingpodcasts, (err, data) => {
+        if (err) throw err;
+        res.json({ data });
+    });
+
+});
+
 const server = app.listen(PORT, () => {
     console.log(`API started on port ${server.address().port}`);
 });
